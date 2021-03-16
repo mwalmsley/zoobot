c7492520eeaa2065f2ed7382b2059fa094397a7b
Mike Walmsley <walsleymk1@gmail.com>
2021-03-16 10:28:44 +0000

    reorder

--- docs/conf.py
+++ docs/conf.py
@@ -9,10 +9,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../zoobot'))  # currently in docs subfolder
 
 
 # -- Project information -----------------------------------------------------
@@ -57,7 +57,3 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 
-# add zoobot itself to environment
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../zoobot'))  # currently in docs subfolder
