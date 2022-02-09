429429b70c71479aa1f4c683f3fe641e650b87ef
Mike Walmsley <walmsleymk1@gmail.com>
2022-02-09 13:21:08 +0000

    try two tasks per node

--- only_for_me/slurm_scripts/train_pytorch_model_multinode.sh
+++ only_for_me/slurm_scripts/train_pytorch_model_multinode.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=pyslur                     # Job name
 #SBATCH --output=pyslur_%A.log 
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task 8
 #SBATCH --mem=0
 #SBATCH --constraint=A100
