f473a2a581a39656b249c2c29b52c534b1fd50c2
Mike Walmsley <walmsleymk1@gmail.com>
2022-02-09 14:03:15 +0000

    set 1 node per task

--- only_for_me/slurm_scripts/train_pytorch_model_manual_cluster.sh
+++ only_for_me/slurm_scripts/train_pytorch_model_manual_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pyslur                     # Job name
 #SBATCH --output=clus_%A_%a.log 
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task 12
 #SBATCH --mem=20gb
