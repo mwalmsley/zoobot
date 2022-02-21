0446f7e864b088378c760f870db3ba5bfc0e4e9f
Mike Walmsley <walmsleymk1@gmail.com>
2022-02-21 17:07:10 +0000

    temporarily revert back to torchvision pending debugging

--- zoobot/pytorch/datasets/decals_dr8.py
+++ zoobot/pytorch/datasets/decals_dr8.py
@@ -3,15 +3,12 @@ import os
 from typing import Optional
 
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from torchvision.io import read_image  # may want to replace with self.read_image for e.g. FITS, Liza
 import torch
 from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
 import pytorch_lightning as pl
-
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 
 # https://pytorch-lightning.readthedocs.io/en/stable/extensions/datamodules.html
 
@@ -21,7 +18,7 @@ class DECALSDR8DataModule(pl.LightningDataModule):
         self,
         schema,
         # provide full catalog for automatic split, or...
-        catalog=None,
+        catalog=None,  
         # provide train/val/test catalogs for your own previous split
         train_catalog=None,
         val_catalog=None,
@@ -54,28 +51,26 @@ class DECALSDR8DataModule(pl.LightningDataModule):
         self.seed = seed
 
         if greyscale:
-            transforms_to_apply = [A.ToGray(p=1)]
+            transforms_to_apply = [transforms.Grayscale()]
         else:
             transforms_to_apply = []
 
         transforms_to_apply += [
             # transforms.RandomCrop(size=(224, 224)),
-            A.Rotate(limit=180, interpolation=1, always_apply=True, border_mode=0, value=0), # anything outside of the original image is set to 0.
-            A.RandomResizedCrop(
-                height=224, # after crop resize
-                width=224,
-                scale=(0.7,0.8), # crop factor
-                ratio=(0.9, 1.1), # crop aspect ratio
-                interpolation=1, # This is "INTER_LINEAR" == BILINEAR interpolation. See: https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html
-                always_apply=True
-            ), # new aspect ratio
-            A.VerticalFlip(p=0.5),
-            ToTensorV2(),
-            #transforms.ConvertImageDtype(torch.float) # No Albumentations equivalent. Data should be in float anyways from interpolation etc.
+            transforms.RandomResizedCrop(
+                size=(224, 224),  # after crop then resize
+                scale=(0.7, 0.8),  # crop factor
+                ratio=(0.9, 1.1),  # crop aspect ratio
+                interpolation=transforms.InterpolationMode.BILINEAR),  # new aspect ratio
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomRotation(degrees=90., interpolation=transforms.InterpolationMode.BILINEAR),
+            # transforms.ToTensor()  # smth in here is already making it a tensor
+            transforms.ConvertImageDtype(torch.float)
             # TODO maybe normalise further? already 0-1 by default it seems which is perfect tbh
         ]
 
-        self.transform = A.Compose(transforms_to_apply)  # TODO more
+        self.transform = transforms.Compose(transforms_to_apply)  # TODO more
 
     def prepare_data(self):
         pass   # could include some basic checks
@@ -102,7 +97,7 @@ class DECALSDR8DataModule(pl.LightningDataModule):
             )
             self.val_dataset = DECALSDR8Dataset(
                 catalog=self.val_catalog, schema=self.schema, transform=self.transform
-            )
+            ) 
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
@@ -138,13 +133,11 @@ class DECALSDR8Dataset(Dataset):
     def __getitem__(self, idx):
         galaxy = self.catalog.iloc[idx]
         img_path = galaxy['file_loc']
-        # Read in torch.tensors using torchvision.io.read_image, convert to np.ndarray. Rearange for colors in the default place for cv2 images HxWxC.
-        # Color order shouldn't matter as we are converting to BW images.
-        image = np.array(read_image(img_path), dtype=np.float64).transpose(1,2,0)
+        image = read_image(img_path)  # torchvision.io.read_image
         label = get_galaxy_label(galaxy, self.schema)
 
         if self.transform:
-            image = self.transform(image=image)['image']
+            image = self.transform(image)
 
         if self.target_transform:
             label = self.target_transform(label)
