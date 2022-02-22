da6bc79c0c57a86e6aedbdbd42dc420b5f09c11f
Mike Walmsley <walmsleymk1@gmail.com>
2022-02-22 21:49:56 +0000

    working well on wandb, a little over 4 mins. try 6 workers

--- only_for_me/slurm_scripts/train_pytorch_model.sh
+++ only_for_me/slurm_scripts/train_pytorch_model.sh
@@ -21,7 +21,7 @@ PYTHON=/share/nas2/walml/miniconda3/envs/zoobot/bin/python
 
 THIS_DIR=/share/nas2/walml/repos/gz-decals-classifiers
 
-EXPERIMENT_DIR=$THIS_DIR/results/early_stopping_2xgpu_greyscale_f16_512_vision_oom_speedtest_prefetch40
+EXPERIMENT_DIR=$THIS_DIR/results/early_stopping_2xgpu_greyscale_f16_512_vision_oom_speedtest_prefetch40_6workers
 
 $PYTHON /share/nas2/walml/repos/zoobot/zoobot/pytorch/examples/train_model.py \
     --experiment-dir $EXPERIMENT_DIR \
