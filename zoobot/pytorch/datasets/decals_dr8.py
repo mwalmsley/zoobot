e05328f5c0ded35d2e14e1991349ae7f7e9986b1
Mike Walmsley <walmsleymk1@gmail.com>
2022-02-22 13:54:05 +0000

    doesn't hang but seems very very slow - 4 min for sanity check.
    Turn off cv2 tweak and set num workers back to 0 for main process

--- zoobot/pytorch/datasets/decals_dr8.py
+++ zoobot/pytorch/datasets/decals_dr8.py
@@ -14,9 +14,9 @@ from torch.utils.data import DataLoader, Dataset
 import pytorch_lightning as pl
 import simplejpeg
 
-import cv2
-cv2.setNumThreads(0)
-cv2.ocl.setUseOpenCL(False)
+# import cv2
+# cv2.setNumThreads(0)
+# cv2.ocl.setUseOpenCL(False)
 
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
