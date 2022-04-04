09f8e38e7628e12587df9b45cac32378487e3f60
Mike Walmsley <walmsleymk1@gmail.com>
2022-04-04 16:39:04 +0100

    return res4 as output

--- zoobot/pytorch/estimators/resnet_detectron2_custom.py
+++ zoobot/pytorch/estimators/resnet_detectron2_custom.py
@@ -1,4 +1,4 @@
-from turtle import shape
+import torch
 from detectron2.config.defaults import _C as default_config
 from detectron2.layers import shape_spec
 
@@ -20,15 +20,20 @@ def get_resnet(
         raise NotImplementedError  # detectron2's version has no top
 
     base_resnet = build_resnet_backbone(default_config, input_shape)  # exactly matching detectron2's version
+    # output is dict of default_config.MODEL.OUT_FEATURES e.g. {'res4': (res4 features)}
 
     return base_resnet
-
-    
 
 
 if __name__ == '__main__':
 
+    import numpy as np
+
     input_shape = shape_spec.ShapeSpec(height=None, width=None, channels=1, stride=None)
 
     base_resnet = build_resnet_backbone(default_config, input_shape)  # exactly matching detectron2's version
-    print(base_resnet)
+    # print(base_resnet)
+
+    x = torch.from_numpy(np.random.rand(64, 1, 224, 224)).float()
+    print(base_resnet(x))
+    print(base_resnet(x).shape)
--- zoobot/pytorch/estimators/resnet_detectron2_standard.py
+++ zoobot/pytorch/estimators/resnet_detectron2_standard.py
@@ -431,22 +431,23 @@ class ResNet(Backbone):
         Returns:
             dict[str->Tensor]: names and the corresponding features
         """
+        # can slightly simplify this as I know I only want the final pre-dense layer, stage=res4
         assert x.dim() == 4, f"ResNet takes an input of shape (N, C, H, W). Got {x.shape} instead!"
         outputs = {}
         x = self.stem(x)
-        if "stem" in self._out_features:
-            outputs["stem"] = x
+        # if "stem" in self._out_features:
+        #     outputs["stem"] = x
         for name, stage in zip(self.stage_names, self.stages):
             x = stage(x)
             if name in self._out_features:
                 outputs[name] = x
-        if self.num_classes is not None:
-            x = self.avgpool(x)
-            x = torch.flatten(x, 1)
-            x = self.linear(x)
-            if "linear" in self._out_features:
-                outputs["linear"] = x
-        return outputs
+        # if self.num_classes is not None:
+        #     x = self.avgpool(x)
+        #     x = torch.flatten(x, 1)
+        #     x = self.linear(x)
+        #     if "linear" in self._out_features:
+        #         outputs["linear"] = x
+        return outputs['res4']  # hacky but fine for now
 
     def output_shape(self):
         return {
