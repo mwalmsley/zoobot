ed76612ae6573bfef6e937d6a6f4d5b88ca8b0e3
Mike Walmsley <walmsleymk1@gmail.com>
2022-04-07 18:02:50 +0100

    works - add color version

--- replication/pytorch/train_model_on_decals_dr5_splits.sh
+++ replication/pytorch/train_model_on_decals_dr5_splits.sh
@@ -35,7 +35,7 @@ GPUS=2
 # GPUS=2
 
 # be sure to add _color if appropriate
-EXPERIMENT_DIR=$RESULTS_DIR/pytorch/dr5/${ARCHITECTURE}_dr5_pytorch
+EXPERIMENT_DIR=$RESULTS_DIR/pytorch/dr5/${ARCHITECTURE}_dr5_pytorch_color
 
 $PYTHON /share/nas2/walml/repos/zoobot/replication/pytorch/train_model_on_decals_dr5_splits.py \
     --experiment-dir $EXPERIMENT_DIR \
@@ -43,6 +43,5 @@ $PYTHON /share/nas2/walml/repos/zoobot/replication/pytorch/train_model_on_decals
     --resize-size 224 \
     --batch-size 512 \
     --gpus 2 \
-    --mixed-precision
-
-    # --color \
+    --mixed-precision \
+    --color
