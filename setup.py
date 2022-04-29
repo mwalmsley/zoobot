39b10e1fea00b6de2717a3bb8cb5fe810be74978
Mike Walmsley <walmsleymk1@gmail.com>
2022-04-29 18:05:03 +0100

    build is unhappy, maybe missing this syntax

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
             'simplejpeg',
             'albumentations',
             'pyro-ppl == 1.8.0',
-            'git+https://github.com/mwalmsley/pytorch-galaxy-datasets.git#egg=pytorch_galaxy_datasets'
+            'pytorch_galaxy_datasets @ git+https://github.com/mwalmsley/pytorch-galaxy-datasets.git#egg=pytorch_galaxy_datasets'
         ],
         'tensorflow': [
             'tensorflow >= 2.8',
