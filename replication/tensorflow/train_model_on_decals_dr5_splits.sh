99aee1d3cf21535047a03414de5000a50ce4bdbd
Mike Walmsley <walmsleymk1@gmail.com>
2022-07-07 21:09:58 +0100

    try new tensorflow replication

--- replication/tensorflow/train_model_on_decals_dr5_splits.sh
+++ replication/tensorflow/train_model_on_decals_dr5_splits.sh
@@ -32,9 +32,9 @@ TEST_DIR=$TFRECORD_DIR/eval_shards
 
 THIS_DIR=/share/nas2/walml/repos/gz-decals-classifiers
 
-EXPERIMENT_DIR=$THIS_DIR/results/tensorflow/dr5/efficientnet_dr5_tensorflow
+EXPERIMENT_DIR=$THIS_DIR/results/tensorflow/dr5/efficientnet_dr5_tensorflow_greyscale
 
-$PYTHON $ZOOBOT_DIR/zoobot/tensorflow/examples/train_model.py \
+$PYTHON $ZOOBOT_DIR/zoobot/tensorflow/examples/train_model_on_shards.py \
     --experiment-dir $EXPERIMENT_DIR \
     --shard-img-size 300 \
     --resize-size 224 \
