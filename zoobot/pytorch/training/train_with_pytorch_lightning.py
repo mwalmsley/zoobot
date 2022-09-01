9a89a0ba3180bd89e8b93d0570373f93a4d6a690
Mike Walmsley <walmsleymk1@gmail.com>
2022-09-01 18:32:54 +0100

    try 1408 rep for b2, 1.1 * 1280

--- zoobot/pytorch/training/train_with_pytorch_lightning.py
+++ zoobot/pytorch/training/train_with_pytorch_lightning.py
@@ -201,7 +201,7 @@ def select_base_architecture_func_from_name(base_architecture):
         representation_dim = 1280
     if base_architecture == 'efficientnet_b2':
         get_architecture = efficientnet_standard.efficientnet_b2
-        representation_dim = 1280
+        representation_dim = 1408
     elif base_architecture == 'resnet_detectron':
         # only import if needed, as requires gpu version of pytorch or throws cuda errors e.g.
         # from detectron2 import _C -> ImportError: libtorch_cuda_cu.so: cannot open shared object file: No such file or directory
