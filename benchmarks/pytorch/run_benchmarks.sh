5a191339ae1a8995f3b81142c2d46691f35d5da2
Mike Walmsley <walmsleymk1@gmail.com>
2022-11-18 11:45:04 +0000

    2 gpu version

--- benchmarks/pytorch/run_benchmarks.sh
+++ benchmarks/pytorch/run_benchmarks.sh
@@ -12,7 +12,7 @@ TRAIN_JOB=/share/nas2/walml/repos/zoobot/benchmarks/pytorch/run_decals_dr5_bench
 # sbatch --job-name=dr5_py_2bhalflr_min_$RANDOM --export=GPUS=1 $TRAIN_JOB
 
 # otherwise full hardware (standard setup) - 2 gpus, mixed precision
-sbatch --job-name=dr5_py_gr_b256halflr_1pgu_$RANDOM --export=MIXED_PRECISION_STRING=--mixed-precision,GPUS=1 $TRAIN_JOB
+sbatch --job-name=dr5_py_gr_b256halflr_2pgu_$RANDOM --export=MIXED_PRECISION_STRING=--mixed-precision,GPUS=2 $TRAIN_JOB
 # sbatch --job-name=dr5_py_co_nobn_$RANDOM --export=MIXED_PRECISION_STRING=--mixed-precision,GPUS=2,COLOR_STRING=--color $TRAIN_JOB
 
 echo 'PyTorch jobs submitted'
