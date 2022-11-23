ea28d8d1ca2c29edfe9fb2f1c77abf6a689958ed
Mike Walmsley <walmsleymk1@gmail.com>
2022-11-23 12:34:06 +0000

    0-5 died

--- benchmarks/pytorch/run_benchmarks.sh
+++ benchmarks/pytorch/run_benchmarks.sh
@@ -9,7 +9,7 @@ TRAIN_JOB=/share/nas2/walml/repos/zoobot/benchmarks/pytorch/run_decals_dr5_bench
 
 # minimal hardware - 1 gpu, no mixed precision
 # (not specifying a string will default to not doing it)
-sbatch --job-name=dr5_py_min_2bhalflr_$RANDOM --export=GPUS=1 $TRAIN_JOB
+# sbatch --job-name=dr5_py_min_2bhalflr_$RANDOM --export=GPUS=1 $TRAIN_JOB
 
 # otherwise full hardware (standard setup) - 2 gpus, mixed precision
 sbatch --job-name=dr5_py_gr_b256halflr_2pgu_$RANDOM --export=MIXED_PRECISION_STRING=--mixed-precision,GPUS=2 $TRAIN_JOB
--- benchmarks/pytorch/run_decals_dr5_benchmark.sh
+++ benchmarks/pytorch/run_decals_dr5_benchmark.sh
@@ -8,7 +8,7 @@
 #SBATCH --exclusive   # only one task per node
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task=24
-#SBATCH --exclude=compute-0-7
+#SBATCH --exclude=compute-0-7,compute-0-5
 pwd; hostname; date
 
 nvidia-smi
