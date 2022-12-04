20606c088af3c6a97e245a84e4c9299d87974687
Mike Walmsley <walmsleymk1@gmail.com>
2022-12-04 20:21:20 +0000

    point to cuda

--- benchmarks/tensorflow/run_decals_dr5_benchmark.sh
+++ benchmarks/tensorflow/run_decals_dr5_benchmark.sh
@@ -11,8 +11,8 @@
 #SBATCH --exclude=compute-0-7,compute-0-6,compute-0-1,compute-0-0
 pwd; hostname; date
 
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/share/apps/cudnn_8_1_0/cuda/lib64
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
+# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/share/apps/cudnn_8_1_0/cuda/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/nas2/walml/miniconda3/envs/zoobot38_tf/lib/
 
 
 nvidia-smi
