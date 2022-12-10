1ebbc301fe5abcf004857effae67223899444ddf
Mike Walmsley <walmsleymk1@gmail.com>
2022-12-10 20:15:21 +0000

    try 64...

--- benchmarks/pytorch/run_decals_dr5_benchmark.sh
+++ benchmarks/pytorch/run_decals_dr5_benchmark.sh
@@ -34,7 +34,7 @@ RESULTS_DIR=/share/nas2/walml/repos/gz-decals-classifiers/results
 EXPERIMENT_DIR=$RESULTS_DIR/benchmarks/pytorch/dr5
 
 ARCHITECTURE='efficientnetb4'
-BATCH_SIZE=32  # 512 is max for single A100 GPU with MP, 256 without MP
+BATCH_SIZE=64  # 512 is max for single A100 GPU with MP, 256 without MP
 
 echo $PYTHON $ZOOBOT_DIR/benchmarks/pytorch/train_model_on_decals_dr5_splits.py \
     --save-dir $EXPERIMENT_DIR/$SLURM_JOB_NAME \
