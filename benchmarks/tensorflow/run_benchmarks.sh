9e0cbb5a73279399766e96d2badb7f2d5c2cbe14
Mike Walmsley <walmsleymk1@gmail.com>
2022-12-10 14:51:00 +0000

    try with all benchmarks

--- benchmarks/tensorflow/run_benchmarks.sh
+++ benchmarks/tensorflow/run_benchmarks.sh
@@ -6,17 +6,17 @@ TRAIN_JOB=/share/nas2/walml/repos/zoobot/benchmarks/tensorflow/run_dataset_bench
 SEED=$RANDOM
 
 # debug mode
-sbatch --job-name=dr5_tf_debug_$SEED --export=DATASET=gz_decals_dr5,GPUS=1,SEED=$SEED,DEBUG_STRING='--debug' $TRAIN_JOB
+# sbatch --job-name=dr5_tf_debug_$SEED --export=DATASET=gz_decals_dr5,GPUS=1,SEED=$SEED,DEBUG_STRING='--debug' $TRAIN_JOB
 
 # minimal hardware - 1 gpu, no mixed precision
 # (not specifying a string will default to not doing it)
-# sbatch --job-name=dr5_tf_min_$SEED --export=DATASET=gz_decals_dr5,GPUS=1,SEED=$SEED $TRAIN_JOB
+sbatch --job-name=dr5_tf_min_$SEED --export=DATASET=gz_decals_dr5,GPUS=1,SEED=$SEED $TRAIN_JOB
 
 # otherwise full hardware (standard setup) - 2 gpus, mixed precision
-# sbatch --job-name=dr5_tf_gr_$SEED --export=DATASET=gz_decals_dr5,MIXED_PRECISION_STRING=--mixed-precision,GPUS=2,SEED=$SEED $TRAIN_JOB
-# sbatch --job-name=dr5_tf_co_$SEED --export=DATASET=gz_decals_dr5,MIXED_PRECISION_STRING=--mixed-precision,GPUS=2,COLOR_STRING=--color,SEED=$SEED $TRAIN_JOB
+sbatch --job-name=dr5_tf_gr_$SEED --export=DATASET=gz_decals_dr5,MIXED_PRECISION_STRING=--mixed-precision,GPUS=2,SEED=$SEED $TRAIN_JOB
+sbatch --job-name=dr5_tf_co_$SEED --export=DATASET=gz_decals_dr5,MIXED_PRECISION_STRING=--mixed-precision,GPUS=2,COLOR_STRING=--color,SEED=$SEED $TRAIN_JOB
 
 # new - all galaxies
-# sbatch --job-name=evo_tf_gr_$SEED --export=DATASET=gz_evo,MIXED_PRECISION_STRING=--mixed-precision,GPUS=2,SEED=$SEED $TRAIN_JOB
+sbatch --job-name=evo_tf_gr_$SEED --export=DATASET=gz_evo,MIXED_PRECISION_STRING=--mixed-precision,GPUS=2,SEED=$SEED $TRAIN_JOB
 
 echo 'TensorFlow jobs submitted'
