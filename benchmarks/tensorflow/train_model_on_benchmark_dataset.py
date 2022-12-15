4f92633251f65826c7fcbce9178bc7277304a180
Mike Walmsley <walmsleymk1@gmail.com>
2022-12-15 16:13:41 +0000

    run evo training on all

--- benchmarks/tensorflow/train_model_on_benchmark_dataset.py
+++ benchmarks/tensorflow/train_model_on_benchmark_dataset.py
@@ -34,12 +34,14 @@ def get_gz_decals_dr5_benchmark_dataset(data_dir, random_state, download):
     return schema, (train_catalog, val_catalog, test_catalog)
 
 
-def get_gz_evo_benchmark_dataset(data_dir, random_state, download=False, debug=False):
+def get_gz_evo_benchmark_dataset(data_dir, random_state, download=False, debug=False, datasets=['gz_desi', 'gz_hubble', 'gz_candels', 'gz2', 'gz_rings']):
 
     from foundation.datasets import mixed  # not yet public. import will fail if you're not me.
 
+    # temporarily, everything *but* hubble
+    # datasets=['gz_desi', 'gz_candels', 'gz2', 'gz_rings']
 
-    _, (temp_train_catalog, temp_val_catalog, _) = mixed.everything_all_dirichlet_with_rings(data_dir, debug, download=download, use_cache=True)
+    _, (temp_train_catalog, temp_val_catalog, _) = mixed.everything_all_dirichlet_with_rings(data_dir, debug, download=download, use_cache=True, datasets=datasets)
     canonical_train_catalog = pd.concat([temp_train_catalog, temp_val_catalog], axis=0)
 
     # here I'm going to ignore the test catalog
