0a26bf4b15b26d8924838141ddb31d388a5a8a7d
Mike Walmsley <walmsleymk1@gmail.com>
2023-01-30 15:25:02 +0000

    remove non-rel dependencies

--- zoobot/tensorflow/stats/dirichlet_stats.py
+++ zoobot/tensorflow/stats/dirichlet_stats.py
@@ -1,12 +1,23 @@
-import json
 import logging
 
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-from zoobot.tensorflow.stats import mixture_stats
+# from zoobot.tensorflow.stats import mixture_stats
+
+
+def dirichlet_prob_of_answers(concentrations, schema):
+    # badly named vs posteriors, actually gives predicted vote fractions of answers...
+    # mean probability (including dropout) of an answer being given. 
+    # concentrations has (batch, answer, ...) shape where extra dims are distributions (e.g. galaxies, answer, model, pass)
+    p_of_answers = []
+    for q in schema.questions:
+        concentrations_by_q = concentrations[:, q.start_index:q.end_index+1, :]
+        p_of_answers.append(DirichletMultinomialEqualMixture(total_votes=1, concentrations=concentrations_by_q).mean().numpy())
+
+    p_of_answers = np.concatenate(p_of_answers, axis=1)
+    return p_of_answers
 
 class EqualMixture():
 
@@ -36,13 +47,16 @@ class EqualMixture():
         return tf.math.exp(self.log_prob(x))
 
     def mean_prob(self, x):
+        # expected value across all distributions - very handy
         prob = self.prob(x)
         return tf.reduce_mean(prob, axis=-1)
 
     def mean_log_prob(self, x):
         return tf.math.log(self.mean_prob(x))
 
-    def mean(self):  # i.e. expected 
+    def mean(self):
+        # the mean of the mean (expected) value of each distribution
+        # aka the mean of the expected vote fractions
         return tf.reduce_mean([dist.mean() for dist in self.distributions], axis=0)
 
     def mean_cdf(self, x):  # as integration is separable 
@@ -50,7 +64,9 @@ class EqualMixture():
         return tf.reduce_mean([dist.cdf(x) for dist in self.distributions], axis=0)
 
     def cdf(self, x):
-        raise NotImplementedError('Can only be calculated with batch_dim shaped x, unlike .prob etc, needs custom implementation')
+        cdf_by_dist = [dist.cdf(x) for dist in self.distributions]
+        return tf.transpose(cdf_by_dist)
+        # raise NotImplementedError('Can only be calculated with batch_dim shaped x, unlike .prob etc, needs custom implementation')
 
 
 
@@ -68,117 +84,40 @@ class DirichletEqualMixture(EqualMixture):
         Args:
             concentrations (np.ndarray): predicted dirichlet parameters for one galaxy, of shape (galaxies, answers, models)
         """
-        self.concentrations = concentrations.astype(np.float32)
+        # flatten trailing dims (e.g. galaxies, answer, model, pass) to (galaxies, answer, distribution)
+        concentrations = concentrations.reshape(concentrations.shape[0], concentrations.shape[1], -1)
+        concentrations = concentrations.astype(np.float32)
+
+        self.concentrations = concentrations
+
         self.n_distributions = self.concentrations.shape[2]
         self.distributions = [
-            tfp.distributions.Dirichlet(concentrations[:, :, n], validate_args=True)
+            tfp.distributions.Dirichlet(self.concentrations[:, :, n], validate_args=True)
             for n in range(self.n_distributions)
         ]
 
-    def entropy_estimate(self):
-        """
-        Returns:
-            np.ndarray: midpoint between entropy bounds
-        """
-        upper = self.entropy_upper_bound()
-        lower = self.entropy_lower_bound()
-        return lower + (upper-lower)/2.  # midpoint between bounds
-
-    def entropy_upper_bound(self):
-        """
-        Returns:
-            np.ndarray: upper bound on entropy. See ``mixture_stats.entropy_upper_bound``.
-        """
-        return np.array([mixture_stats.entropy_upper_bound(galaxy_conc, weights=np.ones(self.n_distributions)/self.n_distributions) for galaxy_conc in self.concentrations])
-
-    def entropy_lower_bound(self):
-        """
-        Returns:
-            np.ndarray: lower bound on entropy. See ``mixture_stats.entropy_lower_bound``.
-        """
-        return np.array([mixture_stats.entropy_lower_bound(galaxy_conc, weights=np.ones(self.n_distributions)/self.n_distributions) for galaxy_conc in self.concentrations])
-
-
-    # def to_beta(self, answer_index, batch_dim):
+    # def entropy_estimate(self):
     #     """
-    #     Convert from Dirichlet (multivariate) to Beta (bivariate) by adding up all the parameters to every answer but one.
-    #     E.g. 3-variate dirichlet(concentrations=[2, 3, 6]) converts to bivariate beta(a=2, b=3+6) for ``answer_index=0``
-    #     Intuitively, this is like going from "is it a, b, or c?" to "is it a or not-a?"
-
-    #     Args:
-    #         answer_index (int): which answer to use as first concentration. Other answers will have concentrations summed.
-    #         batch_dim (int): batch dimension i.e. num dirichlet distributions in this mixture.
-
     #     Returns:
-    #         BetaEqualMixture: beta(answer index, not answer index) with a batch dimension of num. dirichlet distributions in this mixture
+    #         np.ndarray: midpoint between entropy bounds
     #     """
-    #     assert self.batch_shape == 1  # beta uses batch shape for cdf(x), so needs to be able to broadcast, so can only do one galaxy at a time
-    #     answer_concentrations = self.concentrations[:, answer_index]
-    #     # may give index errors
-    #     other_concentrations = self.concentrations[:, :answer_index].sum(axis=1) + self.concentrations[:, answer_index+1:].sum(axis=1)
-    #     beta_concentrations = np.stack([answer_concentrations, other_concentrations], axis=1)
-    #     return BetaEqualMixture(beta_concentrations, batch_dim=batch_dim)
+    #     upper = self.entropy_upper_bound()
+    #     lower = self.entropy_lower_bound()
+    #     return lower + (upper-lower)/2.  # midpoint between bounds
 
+    # def entropy_upper_bound(self):
+    #     """
+    #     Returns:
+    #         np.ndarray: upper bound on entropy. See ``mixture_stats.entropy_upper_bound``.
+    #     """
+    #     return np.array([mixture_stats.entropy_upper_bound(galaxy_conc, weights=np.ones(self.n_distributions)/self.n_distributions) for galaxy_conc in self.concentrations])
 
-
-# class BetaEqualMixture(DirichletEqualMixture):
-
-#     def __init__(self, concentrations, batch_dim):
-#         """
-#         Batch dim is the number of models in the mixture, 
-
-#         Only supports galaxy_dim = 1 i.e. one galaxy at a time.
-    
-#         As with DirichletEqualMixture, self.distributions is list of tfp.distributions.Beta, each with dimension (galaxy, answer)
-
-#         Only used for calculating confidence intervals for a specific answer - which is currently impractically slow anyway.
-
-#         Args:
-#             concentrations ([type]): [description]
-#             batch_dim ([type]): [description]
-#         """
-#         assert concentrations.shape[0] == 1
-#         assert concentrations.shape[1]  == 2
-#         # print(concentrations.shape)
-#         self.batch_dim = batch_dim
-#         self.concentrations = np.stack([np.squeeze(concentrations) for _ in range(batch_dim)], axis=0).astype(np.float32)
-#         # print(self.concentrations.shape)
-#         assert self.concentrations.ndim == 3
-#         self.n_distributions = self.concentrations.shape[2]
-#         self.distributions = [
-#             tfp.distributions.Beta(
-#                 concentration0=self.concentrations[:, 0, n],
-#                 concentration1=self.concentrations[:, 1, n],
-#                 validate_args=True)
-#             for n in range(self.n_distributions)
-#         ]
-
-#     @property
-#     def standard_sample_grid(self):
-#         return np.linspace(1e-8, 1. - 1e-8, num=self.batch_dim)
-
-#     def mean_mode(self):  # approximate only
-#         assert self.batch_dim >= 50
-#         # print(self.mean_prob(self.standard_sample_grid).shape)
-#         mode_index = np.argmax(self.mean_prob(self.standard_sample_grid))
-#         return self.standard_sample_grid[mode_index]  # bit lazy but it works
-
-#     def confidence_interval(self, interval_width):
-#         assert self.batch_dim >= 50
-#         # dist must be unimodal (though the mode can be at extremes)
-#         cdf = self.mean_cdf(self.standard_sample_grid)
-#         mode = self.mean_mode()
-#         # print(mode)
-#         mode_cdf = self.mean_cdf(mode)[0]  # just using the first, batch size broadcasting gives batch_size identical results
-#         # print(mode_cdf)
-#         return confidence_interval_from_cdf(self.standard_sample_grid, cdf, mode_cdf, interval_width)
-
-#     def cdf(self, x):
-#         # for dist in self.distributions:
-#             # dist_with_batch_matching_x = tfp.distributions.Beta()
-#         # print(x)
-#         cdfs = [dist.cdf(x) for dist in self.distributions]
-#         return tf.transpose(cdfs)
+    # def entropy_lower_bound(self):
+    #     """
+    #     Returns:
+    #         np.ndarray: lower bound on entropy. See ``mixture_stats.entropy_lower_bound``.
+    #     """
+    #     return np.array([mixture_stats.entropy_lower_bound(galaxy_conc, weights=np.ones(self.n_distributions)/self.n_distributions) for galaxy_conc in self.concentrations])
 
 
 class DirichletMultinomialEqualMixture(EqualMixture):
@@ -193,36 +132,149 @@ class DirichletMultinomialEqualMixture(EqualMixture):
 
         Args:
             total_votes (np.ndarray): total votes for some question, of shape (galaxies)
-            concentrations (np.ndarray): of shape (galaxies, answer, model). Answers for one question only due to total votes.
+            concentrations (np.ndarray): of shape (galaxies, answer, ...). Answers for *one question only* due to total votes. Extra dims flattened.
         """
         
         self.total_votes = np.array(total_votes).astype(np.float32)
+
+        # flatten trailing dims (e.g. galaxies, answer, model, pass) to (galaxies, answer, distribution)
+        concentrations = concentrations.reshape(concentrations.shape[0], concentrations.shape[1], -1)
         self.concentrations = concentrations.astype(np.float32)
+
         self.n_distributions = self.concentrations.shape[2]
         self.distributions = [
             tfp.distributions.DirichletMultinomial(self.total_votes, self.concentrations[:, :, n], validate_args=True)
             for n in range(self.n_distributions)
         ]
 
-def load_all_concentrations(df, concentration_cols):
-    temp = []
-    for col in concentration_cols:
-        temp.append(np.stack(df[col].apply(json.loads).values, axis=0))
-    return np.stack(temp, axis=2).transpose(0, 2, 1)
 
 
-def dirichlet_prob_of_answers(concentrations, schema, temperature=None):
-    # badly named vs posteriors, actually gives predicted vote fractions of answers...
-    # mean probability (including dropout) of an answer being given. 
-    # concentrations has (batch, answer, dropout) shape
-    p_of_answers = []
-    for q in schema.questions:
-        concentrations_by_q = concentrations[:, q.start_index:q.end_index+1, :]
-        p_of_answers.append(DirichletMultinomialEqualMixture(total_votes=1, concentrations=concentrations_by_q).mean().numpy())
+    def to_beta(self, answer_index, batch_dim):
+        """
+        Convert from Dirichlet (multivariate) to Beta (bivariate) by adding up all the parameters to every answer but one.
+        E.g. 3-variate dirichlet(concentrations=[2, 3, 6]) converts to bivariate beta(a=2, b=3+6) for ``answer_index=0``
+        Intuitively, this is like going from "is it a, b, or c?" to "is it a or not-a?"
 
-    p_of_answers = np.concatenate(p_of_answers, axis=1)
-    return p_of_answers
+        Args:
+            answer_index (int): which answer to use as first concentration. Other answers will have concentrations summed.
+            batch_dim (int): batch dimension i.e. num dirichlet distributions in this mixture.
 
+        Returns:
+            BetaEqualMixture: beta(answer index, not answer index) with a batch dimension of num. dirichlet distributions in this mixture
+        """
+        assert self.batch_shape == 1  # beta uses batch shape for cdf(x), so needs to be able to broadcast, so can only do one galaxy at a time
+        num_answers = 
+        answer_concentrations = self.concentrations[:, answer_index]
+        all_concentrations = self.concentrations.reshape(1, sum(axis=1)
+        beta_concentrations = np.stack([answer_concentrations, other_concentrations], axis=1)
+        return BetaEqualMixture(beta_concentrations, batch_dim=batch_dim)
+
+
+
+class BetaEqualMixture(DirichletEqualMixture):
+
+    def __init__(self, concentrations):
+        """
+        Batch dim is the number of models in the mixture, 
+
+        Only supports galaxy_dim = 1 i.e. one galaxy at a time.
+    
+        As with DirichletEqualMixture, self.distributions is list of tfp.distributions.Beta, each with dimension (galaxy, answer)
+
+        Only used for calculating confidence intervals for a specific answer - which is currently impractically slow anyway.
+
+        Args:
+            concentrations ([type]): of shape (1, 2, ...) for 1 galaxy, 2 answers, and trailing distibution dims
+        """
+        assert concentrations.shape[0] == 1  # 1 galaxy
+        assert concentrations.shape[1] == 2  # 2 answers (i.e. "this answer" or "not this answer")
+        # remaining dimensions assumed to be distributions
+        concentrations = concentrations.reshape(concentrations.shape[0], concentrations.shape[1], -1)
+        # print(concentrations.shape)
+        
+        self.batch_dim = batch_dim
+        self.concentrations = np.stack([np.squeeze(concentrations) for _ in range(batch_dim)], axis=0).astype(np.float32)
+        # print(self.concentrations.shape)
+        assert self.concentrations.ndim == 3
+        self.n_distributions = self.concentrations.shape[2]
+        self.distributions = [
+            tfp.distributions.Beta(
+                concentration0=self.concentrations[:, 0, n],
+                concentration1=self.concentrations[:, 1, n],
+                validate_args=True)
+            for n in range(self.n_distributions)
+        ]
+
+    @property
+    def standard_sample_grid(self):
+        return np.linspace(1e-8, 1. - 1e-8, num=self.batch_dim)
+
+    def mean_mode(self):  # approximate only
+        assert self.batch_dim >= 50
+        # print(self.mean_prob(self.standard_sample_grid).shape)
+        mode_index = np.argmax(self.mean_prob(self.standard_sample_grid))
+        return self.standard_sample_grid[mode_index]  # bit lazy but it works
+
+    def confidence_interval(self, interval_width):
+        assert self.batch_dim >= 50
+        # dist must be unimodal (though the mode can be at extremes)
+        cdf = self.mean_cdf(self.standard_sample_grid)
+        mode = self.mean_mode()
+        # print(mode)
+        mode_cdf = self.mean_cdf(mode)[0]  # just using the first, batch size broadcasting gives batch_size identical results
+        # print(mode_cdf)
+        return confidence_interval_from_cdf(self.standard_sample_grid, cdf, mode_cdf, interval_width)
+
+    def cdf(self, x):
+        # for dist in self.distributions:
+            # dist_with_batch_matching_x = tfp.distributions.Beta()
+        # print(x)
+        cdfs = [dist.cdf(x) for dist in self.distributions]
+        return tf.transpose(cdfs)
+
+
+def confidence_interval_from_cdf(x, cdf, mode_cdf, interval_width):
+    width_per_half = interval_width / 2
+
+    lower_index = np.argmin(np.abs(mode_cdf  - cdf - width_per_half))
+    upper_index = np.argmin(np.abs(cdf - mode_cdf - width_per_half))
+
+    if mode_cdf < width_per_half:  # i.e. if lower interval hits the edge
+        upper_index = np.argmin(np.abs(cdf - interval_width))
+    elif 1 - mode_cdf < width_per_half:  # i.e. upper interval hits the edge
+        lower_index = np.argmin(np.abs(1 - cdf - interval_width))
+
+    assert np.allclose(cdf[upper_index] - cdf[lower_index], interval_width, atol=.02)
+
+    lower = x[lower_index]
+    upper = x[upper_index]
+    return lower, upper
+
+if __name__ == '__main__':
+
+    concentrations = np.random.rand(10, 3) * 100 + 1  # 10 events/galaxies, 3 possible outcomes/answers
+
+    # total_votes = 10
+    # votes_except_last = np.random.randint(0, int(total_votes/2), size=(10, 2))  # can be no more than half, for convenience
+    # votes_last = total_votes - votes_except_last.sum(axis=1)
+    # votes = np.concatenate([votes_except_last, votes_last.reshape(-1, 1)], axis=1)
+
+    # concentrations = np.random.rand(10, 3, 2, 5) * 100 + 1  # 10 events/galaxies, 3 possible outcomes/answers, 2 models, 5 forward passes
+
+    # mixture = DirichletMultinomialEqualMixture(total_votes=total_votes, concentrations=concentrations)
+    # print(mixture.mean())
+    # print(mixture.mean().shape)  # taking a mean across distributions. not totally sure this is correct approach
+
+    # eval_points_except_last = np.random.rand(*concentrations.shape) / 3
+    # eval_points_last = 1 - eval_points_except_last.sum(axis=1)
+    # eval_points = np.concatenate([eval_points_except_last, eval_points_last.reshape(-1, 1)], axis=1)
+
+    # mixture = DirichletEqualMixture(concentrations=concentrations)
+
+    # print(mixture.cdf(eval_points))
+
+    mixture = tfp.distributions.Beta(10., 3.)
+    print(mixture.cdf(.8))
 
 
 # # only used for posthoc evaluation, not when training
@@ -272,21 +324,3 @@ def dirichlet_prob_of_answers(concentrations, schema, temperature=None):
 #     return confidence_interval_from_cdf(x, cdf, mode_cdf, interval_width)
 
 
-# def confidence_interval_from_cdf(x, cdf, mode_cdf, interval_width):
-#     width_per_half = interval_width / 2
-
-#     lower_index = np.argmin(np.abs(mode_cdf  - cdf - width_per_half))
-#     upper_index = np.argmin(np.abs(cdf - mode_cdf - width_per_half))
-
-#     if mode_cdf < width_per_half:  # i.e. if lower interval hits the edge
-#         remaining_cdf = mode_cdf  # b
-#         upper_index = np.argmin(np.abs(cdf - interval_width))
-#     elif 1 - mode_cdf < width_per_half:  # i.e. upper interval hits the edge
-#         remaining_cdf = mode_cdf  # b
-#         lower_index = np.argmin(np.abs(1 - cdf - interval_width))
-
-#     assert np.allclose(cdf[upper_index] - cdf[lower_index], interval_width, atol=.02)
-
-#     lower = x[lower_index]
-#     upper = x[upper_index]
-#     return lower, upper
