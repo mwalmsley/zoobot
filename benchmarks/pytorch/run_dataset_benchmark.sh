63e081d2ec3084edc39be32d957bfd35c2c6e7fb
Mike Walmsley <walmsleymk1@gmail.com>
2023-03-01 15:17:30 -0800

    and convnext

--- benchmarks/pytorch/run_dataset_benchmark.sh
+++ benchmarks/pytorch/run_dataset_benchmark.sh
@@ -25,8 +25,7 @@ pwd; hostname; date
 nvidia-smi
 
 export NCCL_DEBUG=INFO
-
-PYTORCH_KERNEL_CACHE_PATH=/share/nas2/walml/.cache/torch/kernels
+export PYTORCH_KERNEL_CACHE_PATH=/share/nas2/walml/.cache/torch/kernels
 
 ZOOBOT_DIR=/share/nas2/walml/repos/zoobot
 PYTHON=/share/nas2/walml/miniconda3/envs/zoobot38_torch/bin/python
