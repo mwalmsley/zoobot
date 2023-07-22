3819e42bb91c14455682f775b52ec4481da4a019
Mike Walmsley <walmsleymk1@gmail.com>
2023-07-22 14:48:35 +0100

    remake evo dataset

--- zoobot/shared/benchmark_datasets.py
+++ zoobot/shared/benchmark_datasets.py
@@ -31,7 +31,8 @@ def get_gz_evo_benchmark_dataset(data_dir, random_state, download=False, debug=F
     # datasets = ['gz_desi', 'gz_candels', 'gz2', 'gz_rings']
     datasets = ['gz_desi', 'gz_candels', 'gz_hubble', 'gz2', 'gz_rings', 'gz_cosmic_dawn']
 
-    _, (temp_train_catalog, temp_val_catalog, _) = mixed.everything_all_dirichlet_with_rings(data_dir, debug, download=download, use_cache=True, datasets=datasets)
+    # TODO temporarily no cache, to remake
+    _, (temp_train_catalog, temp_val_catalog, _) = mixed.everything_all_dirichlet_with_rings(data_dir, debug, download=download, use_cache=False, datasets=datasets)
     canonical_train_catalog = pd.concat([temp_train_catalog, temp_val_catalog], axis=0)
 
     # here I'm going to ignore the test catalog
