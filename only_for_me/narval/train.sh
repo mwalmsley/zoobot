6118ee0fd00387e8016c9fc2eb8ed77253c293b5
Mike Walmsley <walmsleymk1@gmail.com>
2023-11-03 17:29:10 -0400

    non-gpu version

---
+++ only_for_me/narval/train.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --time=0:20:0  
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=4
+
+### SBATCH --gres=gpu:a100:1
+
+nvidia-smi
+
+PYTHON=/home/walml/envs/zoobot39_dev/bin/python
+
+cp -r /project/def-bovy/walml/data/roots/galaxy_mnist $SLURM_TMPDIR/walml/finetune/data/
+
+ls $SLURM_TMPDIR/walml/finetune/data/galaxy_mnist
+
+export NCCL_BLOCKING_WAIT=1  #Set this environment variable if you wish to use the NCCL backend for inter-GPU communication.
+# export MASTER_ADDR=$(hostname) #Store the master node’s IP address in the MASTER_ADDR environment variable.
+# echo "r$SLURM_NODEID master: $MASTER_ADDR"
+# echo "r$SLURM_NODEID Launching python script"
+
+REPO_DIR=/project/def-bovy/walml/zoobot/
+srun $PYTHON $REPO_DIR/only_for_me/narval/train.py --save-dir $REPO_DIR/only_for_me/narval/debug_models --batch-size 4 --color --debug
+# srun python $SLURM_TMPDIR/zoobot/only_for_me/narval/finetune.py
