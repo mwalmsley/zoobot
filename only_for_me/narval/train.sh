45d836c35b067fff06007c147b3a9faba6d1ce3f
Mike Walmsley <walmsleymk1@gmail.com>
2023-11-07 11:49:48 -0500

    2 gpu scaling

--- only_for_me/narval/train.sh
+++ only_for_me/narval/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --time=2:50:0  
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=2
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu 4G
-#SBATCH --gres=gpu:v100:4
+#SBATCH --gres=gpu:v100:2
 
 nvidia-smi
 
