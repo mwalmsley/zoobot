be5dda8a5e523e13a03d7d240a18615d49f2eb83
Mike Walmsley <walmsleymk1@gmail.com>
2023-11-23 17:55:56 -0500

    effnet v2 on 1 gpu

--- only_for_me/narval/train.sh
+++ only_for_me/narval/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --time=23:30:0  
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu 4G
-#SBATCH --gres=gpu:v100:4
+#SBATCH --gres=gpu:v100:1
 
 nvidia-smi
 
@@ -25,12 +25,22 @@ REPO_DIR=/project/def-bovy/walml/zoobot
 #     --num-workers 10 \
 #     --color --wandb --mixed-precision --compile-encoder
 
+
+# batch sizes
+# v100
+# effnet b0 256
+# maxvittiny_rw_224 64
+# tf_efficientnetv2_b0 256?
+# tf_efficientnetv2_s 64?
+# pit_xs_224 256?
+# pit_s_224 64?
+
 srun $PYTHON $REPO_DIR/only_for_me/narval/train.py \
-    --save-dir $REPO_DIR/only_for_me/narval/desi_300px_maxvittiny_rw_224_4gpu \
-    --batch-size 64 \
-    --gpus 4 \
+    --save-dir $REPO_DIR/only_for_me/narval/desi_300px_tf_efficientnetv2_b0_4gpu \
+    --batch-size 256 \
+    --gpus 1 \
     --num-workers 10 \
-    --architecture maxvit_tiny_rw_224 \
+    --architecture tf_efficientnetv2_b0 \
     --color --wandb --mixed-precision --compile-encoder
 
     # maxvit_small_tf_224 \
