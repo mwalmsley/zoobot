34a19f37870578a508162f5edd3a60c1312cb41e
Mike Walmsley <walmsleymk1@gmail.com>
2023-11-23 21:43:37 -0500

    effnet b2

--- only_for_me/narval/train.sh
+++ only_for_me/narval/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --time=23:30:0  
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu 4G
-#SBATCH --gres=gpu:v100:1
+#SBATCH --gres=gpu:v100:4
 
 nvidia-smi
 
@@ -41,13 +41,14 @@ REPO_DIR=/project/def-bovy/walml/zoobot
 # https://huggingface.co/timm/convnextv2_nano.fcmae TODO with MAE
 # convnext_nano
 # convnext_tiny
+# efficientnet_b2 64
 
 srun $PYTHON $REPO_DIR/only_for_me/narval/train.py \
-    --save-dir $REPO_DIR/only_for_me/narval/desi_300px_convnext_tiny_1gpu \
-    --batch-size 128 \
-    --gpus 1 \
+    --save-dir $REPO_DIR/only_for_me/narval/desi_300px_efficientnet_b2_4gpu \
+    --batch-size 64 \
+    --gpus 4 \
     --num-workers 10 \
-    --architecture convnext_tiny \
+    --architecture efficientnet_b2 \
     --color --wandb --mixed-precision --compile-encoder
 
     # maxvit_small_tf_224 \
