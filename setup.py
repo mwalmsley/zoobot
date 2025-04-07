4a5341bf5d85b011f0bb445a6c9725b644c1c299
Mike Walmsley <walmsleymk1@gmail.com>
2025-04-07 10:57:49 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="zoobot",
-    version="2.0.1",
+    version="2.0.2",
     author="Mike Walmsley",
     author_email="walmsleymk1@gmail.com",
     description="Galaxy morphology classifiers",
